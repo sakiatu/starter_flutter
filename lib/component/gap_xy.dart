920c235a7f9e67cd9a8cdd273cc4caf35725c7fe
Sakib <sakib.active@gmail.com>
2024-02-02 22:39:45 +0600

    create gap_xy widget

---
+++ lib/component/gap_xy.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+import '../const/size.dart';
+
+class GapXY extends SizedBox {
+  const GapXY([double x = Gap.regular, double y = Gap.regular]) : super(width: x, height: y, key: null);
+}
+
+class GapX extends GapXY {
+  const GapX([double x = Gap.regular]) : super(x, 0);
+}
+
+class GapY extends GapXY {
+  const GapY([double y = Gap.regular]) : super(0, y);
+}
