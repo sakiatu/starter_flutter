1b73003d63c40ef7b20d2b359570a830253d8fca
Sakib <sakib.active@gmail.com>
2024-02-02 22:54:08 +0600

    create loader and loader page widget

---
+++ lib/component/loader.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '../helper/extension/ext_context.dart';
+
+class Loader extends StatelessWidget {
+  final double? size;
+  final double stroke;
+  final bool danger;
+
+  const Loader({this.size, this.stroke = 4.0, this.danger = false}) : super(key: null);
+
+  @override
+  Widget build(BuildContext context) => Center(
+        child: SizedBox(
+            height: size,
+            width: size,
+            child: CircularProgressIndicator(strokeWidth: stroke, color: danger ? context.colors.error : null)),
+      );
+}
+
+
+class LoaderPage extends StatelessWidget {
+  final bool danger;
+
+  const LoaderPage({this.danger = false}) : super(key: null);
+
+  @override
+  Widget build(BuildContext context) => Scaffold(body: Loader(danger: danger));
+}
